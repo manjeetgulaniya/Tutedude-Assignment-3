2f4be69e0358569a05e1379b3d50c7003278dbaa
Manjeet Gulaniya <145897864+manjeetgulaniya@users.noreply.github.com>
2025-09-18 18:33:52 +0000

    Task Done

---
+++ task2.py
@@ -0,0 +1,11 @@
+import math as m 
+num=int(input("Enter the number:\t"))
+
+sqroot=m.sqrt(num)
+print(f"Square Root:\t{sqroot}")
+
+log=m.log(num)
+print(f"Logaritm\t{log}")
+
+sineh = m.sin(num)
+print(f"Sine:\t{sineh}")
